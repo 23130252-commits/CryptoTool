3b3b22be5800fa543f9a3e2374f83ef9a3d83046
Nguyễn Thiên Phước <23130252@st.hcmuaf.edu.vn>
2026-05-14 18:10:36 +0700

    Add HexUtil.java - Utility for HEX conversion

---
+++ src/main/java/com/cryptotool/util/HexUtil.java
@@ -0,0 +1,27 @@
+package com.cryptotool.util;
+
+public class HexUtil {
+    /**
+     * Chuyển byte array sang chuỗi HEX
+     */
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Chuyển chuỗi HEX sang byte array
+     */
+    public static byte[] hexToBytes(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                    + Character.digit(hex.charAt(i + 1), 16));
+        }
+        return data;
+    }
+}
